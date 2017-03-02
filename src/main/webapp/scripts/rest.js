e3e0a8ab893e04a11c62a546484273ac01f180b7
Janusz Tomasik <jtomasik@guidewire.com>
2017-03-02 15:03:41 +0100

    adding more fancy jquery

--- src/main/webapp/scripts/rest.js
+++ src/main/webapp/scripts/rest.js
@@ -1,10 +1,26 @@
-$("#rankButton").click(function() {
-  console.log("elo")
-  $.ajax({
-    url: '/ranking',
-    type: 'GET',
-    success: function(response) {
-      console.log(response)
-    }
+$(document).ready(function() {
+  $("#backToRankButton").click(function() {
+    console.log("elo")
+    $("#li-tab-rank").click()
+    $.ajax({
+      url: '/ranking',
+      type: 'GET',
+      success: function(response) {
+        console.log(response)
+      }
+    });
+  });
+  $(".player-row").click(function(event) {
+    console.log("table click")
+    const playerId = event.currentTarget.id
+    console.log(playerId)
+    $("#li-tab-player-stats").click()
+    $.ajax({
+      url: '/player/'+playerId,
+      type: 'GET',
+      success: function(response) {
+        console.log(response)
+      }
+    });
   });
 });
