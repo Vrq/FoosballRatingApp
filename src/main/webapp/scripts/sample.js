e91160ab9242917e2e67e41c027cad235d32503a
Anastasiia Onchukova <aonchukova@guidewire.com>
2017-03-03 14:41:39 +0100

    Added post to games

--- src/main/webapp/scripts/sample.js
+++ src/main/webapp/scripts/sample.js
@@ -3,17 +3,44 @@ var element = ".form-control";
 var gamescore = ".score-input";
 var ratingupdate = ".rating-update";
 var button_dashboard = "#button-dashboard";
+var player1 = "#player1";
 $(document).ready(function () {
     $(ratingupdate).hide();
     $(button_save).click(function () {
-       $(element).prop('disabled', true);
-       $(gamescore).hide();
+        $(element).prop('disabled', true);
+        $(gamescore).hide();
         $(ratingupdate).show();
-        console.log('siema')
-        console.log($("#player1").find(':selected').text())
+        var player1=$("#player1").find(':selected').text();
+        var player2=$("#player2").find(':selected').text();
+        var player3=$("#player3").find(':selected').text();
+        var player4=$("#player4").find(':selected').text();
+        var json = {
+            aPlayer1: player1,
+            aPlayer2: player2,
+            bPlayer1: player3,
+            bPlayer2: player4,
+            aScore: $("#score1").find(':selected').text(),
+            bScore: $("#score2").find(':selected').text()
+        };
+        $.ajax({
+            url: '/games/insertGame',
+            type: 'POST',
+            data: JSON.stringify(json),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data){
+                console.log("OK");
+            },
+            failure: function() {
+                console.log("FAIL");
+            }
+        });
+        $("#username1").text(player1);
+        $("#username2").text(player2);
+        $("#username3").text(player3);
+        $("#username4").text(player4);
     });
     $(button_dashboard).click(function () {
         window.location.href = 'index.html';
     });
-
 });
