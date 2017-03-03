994313bdc7f35f9e9d420d72fa7c8407655b80eb
Anastasiia Onchukova <aonchukova@guidewire.com>
2017-03-03 11:13:38 +0100

    Update players list from sql

--- src/main/webapp/scripts/rest.js
+++ src/main/webapp/scripts/rest.js
@@ -1,51 +1,51 @@
-$(document).ready(function() {
-  $.ajax({
-  url: '/players/getAll',
-  type: 'GET',
-  success: function(response) {
-    $("#rank-table").find("tr:gt(0)").remove();
-    var rankNo = 1;
-    for(playerRow of response) {
-      $('#rank-table tr:last').after("<tr class='player-row' id='"+playerRow.username+"'><td>"+rankNo+"</td><td>"+playerRow.username+"</td><td>"+playerRow.points+"</td><td>"+playerRow.gamesWon+"</td><td>"+playerRow.gamesLost+"</td><td>"+playerRow.setsWon+" : "+playerRow.setsLost+"</td></tr>");
-      rankNo++;
-    }
-  }
-  });
-
-  $("#backToRankButton").click(function() {
-    $("#li-tab-rank").click()
-  });
-
-  $("#li-tab-rank").click(function() {
-    console.log("elo")
+$(document).ready(function () {
     $.ajax({
-    url: '/players/getAll',
-    type: 'GET',
-    success: function(response) {
-      $("#rank-table").find("tr:gt(0)").remove();
-      var rankNo = 1;
-      for(playerRow of response) {
-        $('#rank-table tr:last').after("<tr class='player-row' id='"+playerRow.username+"'><td>"+rankNo+"</td><td>"+playerRow.username+"</td><td>"+playerRow.points+"</td><td>"+playerRow.gamesWon+"</td><td>"+playerRow.gamesLost+"</td><td>"+playerRow.setsWon+" : "+playerRow.setsLost+"</td></tr>");
-        rankNo++;
-      }
-    }
+        url: '/players/getAll',
+        type: 'GET',
+        success: function (response) {
+            $("#rank-table").find("tr:gt(0)").remove();
+            var rankNo = 1;
+            for (playerRow of response) {
+                $('#rank-table tr:last').after("<tr class='player-row' id='" + playerRow.username + "'><td>" + rankNo + "</td><td>" + playerRow.username + "</td><td>" + playerRow.points + "</td><td>" + playerRow.gamesWon + "</td><td>" + playerRow.gamesLost + "</td><td>" + playerRow.setsWon + " : " + playerRow.setsLost + "</td></tr>");
+                rankNo++;
+                $('.players').append($("<option></option>").text(playerRow.username));
+            }
+        }
     });
-  });
 
-  $("body").on("click",".player-row",function(event) {
-    console.log("table click")
-    const playerId = event.currentTarget.id
-    console.log(playerId)
-    $('#player-stats-div').empty();
-    $("#li-tab-player-stats").click()
-    $.ajax({
-      url: '/players/getByUsername?username='+playerId,
-      type: 'GET',
-      success: function(response) {
-        console.log(response)
-        $('#player-stats-div').append("<h4>Player: "+response.username+"</h4><h4>Rating: "+response.points+"</h4><h4>Games won: "+response.gamesWon+"</h4><h4>Games lost: "+response.gamesLost+"</h4>")
-      }
+    $("#backToRankButton").click(function () {
+        $("#li-tab-rank").click()
     });
-  });
 
+    $("#li-tab-rank").click(function () {
+        console.log("elo")
+        $.ajax({
+            url: '/players/getAll',
+            type: 'GET',
+            success: function (response) {
+                $("#rank-table").find("tr:gt(0)").remove();
+                var rankNo = 1;
+                for (playerRow of response) {
+                    $('#rank-table tr:last').after("<tr class='player-row' id='" + playerRow.username + "'><td>" + rankNo + "</td><td>" + playerRow.username + "</td><td>" + playerRow.points + "</td><td>" + playerRow.gamesWon + "</td><td>" + playerRow.gamesLost + "</td><td>" + playerRow.setsWon + " : " + playerRow.setsLost + "</td></tr>");
+                    rankNo++;
+                }
+            }
+        });
+    });
+
+    $("body").on("click", ".player-row", function (event) {
+        console.log("table click")
+        const playerId = event.currentTarget.id
+        console.log(playerId)
+        $('#player-stats-div').empty();
+        $("#li-tab-player-stats").click()
+        $.ajax({
+            url: '/players/getByUsername?username=' + playerId,
+            type: 'GET',
+            success: function (response) {
+                console.log(response)
+                $('#player-stats-div').append("<h4>Player: " + response.username + "</h4><h4>Rating: " + response.points + "</h4><h4>Games won: " + response.gamesWon + "</h4><h4>Games lost: " + response.gamesLost + "</h4>")
+            }
+        });
+    });
 });
