2787bcc2b3388349db488c39cd9749436aedd944
lmitka <lmitka@guidewire.com>
2017-03-03 13:22:30 +0100

    Update player stats

--- src/main/java/com/guidewire/foosballrating/service/RatingServiceImpl.java
+++ src/main/java/com/guidewire/foosballrating/service/RatingServiceImpl.java
@@ -8,6 +8,8 @@ import com.guidewire.foosballrating.engine.RatingCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class RatingServiceImpl implements RatingService {
 
@@ -21,10 +23,22 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public void updateRatings(Game game) {
-        updatePlayerRating(playerService.getPlayer(game.getaPlayer1()), game);
-        updatePlayerRating(playerService.getPlayer(game.getaPlayer2()), game);
-        updatePlayerRating(playerService.getPlayer(game.getbPlayer1()), game);
-        updatePlayerRating(playerService.getPlayer(game.getbPlayer2()), game);
+        Player aPlayer1 = playerService.getPlayer(game.getaPlayer1());
+        updatePlayerStats(aPlayer1, game.getaScore(), game.getbScore());
+
+        Player aPlayer2 = playerService.getPlayer(game.getaPlayer2());
+        updatePlayerStats(aPlayer2, game.getaScore(), game.getbScore());
+
+        Player bPlayer1 = playerService.getPlayer(game.getbPlayer1());
+        updatePlayerStats(bPlayer1, game.getbScore(), game.getaScore());
+
+        Player bPlayer2 = playerService.getPlayer(game.getbPlayer2());
+        updatePlayerStats(bPlayer2, game.getbScore(), game.getaScore());
+
+        updatePlayerRating(aPlayer1, game);
+        updatePlayerRating(aPlayer2, game);
+        updatePlayerRating(bPlayer1, game);
+        updatePlayerRating(bPlayer2, game);
     }
 
     private void updatePlayerRating(Player player, Game game) {
@@ -33,5 +47,23 @@ public class RatingServiceImpl implements RatingService {
         int rank = playerService.getAllPlayers().lastIndexOf(player) + 1;
         Score score = new Score(player.getUsername(), rank , player.getPoints());
         scoreService.insertScore(score);
+    }
+
+    private void updatePlayerStats(Player player, Integer setsWon, Integer setsLost) {
+        Integer updatedSetsWon = Optional.ofNullable(player.getSetsWon()).orElse(0) + setsWon;
+        Integer updatedSetsLost = Optional.ofNullable(player.getSetsLost()).orElse(0) + setsLost;
+
+        player.setSetsWon(updatedSetsWon);
+        player.setSetsLost(updatedSetsLost);
+
+        if (setsWon > setsLost) { // Win
+            player.setGamesWon((Optional.ofNullable(player.getGamesWon()).orElse(0) + 1));
+        }
+
+        if (setsWon < setsLost) { // Lose
+            player.setGamesLost((Optional.ofNullable(player.getGamesLost()).orElse(0) + 1));
+        }
+
+        // Draw
     }
 }
