580478b5394a1f70103b3bff78574d191505905f
Janusz Tomasik <jtomasik@guidewire.com>
2017-03-03 09:16:01 +0100

    update player stats

--- src/main/webapp/scripts/rest.js
+++ src/main/webapp/scripts/rest.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  $.ajax({
+  url: '/players/getAll',
+  type: 'GET',
+  success: function(response) {
+    $("#rank-table").find("tr:gt(0)").remove();
+    var rankNo = 1;
+    for(playerRow of response) {
+      $('#rank-table tr:last').after("<tr class='player-row' id='"+playerRow.username+"'><td>"+rankNo+"</td><td>"+playerRow.username+"</td><td>"+playerRow.points+"</td><td>"+playerRow.gamesWon+"</td><td>"+playerRow.gamesLost+"</td><td>"+playerRow.setsWon+" : "+playerRow.setsLost+"</td></tr>");
+      rankNo++;
+    }
+  }
+  });
+
   $("#backToRankButton").click(function() {
     $("#li-tab-rank").click()
   });
@@ -23,12 +36,14 @@ $(document).ready(function() {
     console.log("table click")
     const playerId = event.currentTarget.id
     console.log(playerId)
+    $('#player-stats-div').empty();
     $("#li-tab-player-stats").click()
     $.ajax({
       url: '/players/getByUsername?username='+playerId,
       type: 'GET',
       success: function(response) {
         console.log(response)
+        $('#player-stats-div').append("<h4>Player: "+response.username+"</h4><h4>Rating: "+response.points+"</h4><h4>Games won: "+response.gamesWon+"</h4><h4>Games lost: "+response.gamesLost+"</h4>")
       }
     });
   });
