31f5d08d68556f8bb69b0f02e2dea4ce2ec457ce
Anna Jagodzinska <ajagodzinska@guidewire.com>
2017-03-03 10:51:01 +0100

    score mapper implementation

--- src/main/java/com/guidewire/foosballrating/domain/Score.java
+++ src/main/java/com/guidewire/foosballrating/domain/Score.java
@@ -1,7 +1,54 @@
 package com.guidewire.foosballrating.domain;
 
-/**
- * Created by lmitka on 03/03/2017.
- */
+import java.util.Date;
+
 public class Score {
+
+    private int id;
+    private String username;
+    private int rank;
+    private int points;
+    private Date creationTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+
 }
--- src/main/java/com/guidewire/foosballrating/service/ScoreServiceImpl.java
+++ src/main/java/com/guidewire/foosballrating/service/ScoreServiceImpl.java
@@ -1,8 +1,9 @@
 package com.guidewire.foosballrating.service;
 
 import com.guidewire.foosballrating.domain.Score;
+import com.guidewire.foosballrating.persistence.ScoreMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Date;
 import java.util.List;
@@ -10,18 +11,21 @@ import java.util.List;
 @Service
 public class ScoreServiceImpl implements ScoreService {
 
+    @Autowired
+    ScoreMapper scoreMapper;
+
     @Override
     public List<Score> getAllScoreForPlayer(String playerName) {
-        throw new NotImplementedException();
+        return scoreMapper.getAllScores(playerName);
     }
 
     @Override
     public Score getLatestScoreForPlayer(String playerName) {
-        throw new NotImplementedException();
+        return scoreMapper.getLatestScoreForPlayer(playerName);
     }
 
     @Override
     public List<Score> getScoreForPlayerInTime(String playerName, Date startDate, Date endDate) {
-        throw new NotImplementedException();
+        return scoreMapper.getScoresBetweenDates(playerName, startDate, endDate);
     }
 }
